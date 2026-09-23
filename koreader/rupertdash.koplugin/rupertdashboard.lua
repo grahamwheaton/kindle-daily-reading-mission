e48e04455c690fe91117c67af7ed711d8390408c
grahamwheaton <71554214+grahamwheaton@users.noreply.github.com>
2026-09-23 23:28:33 +0100

    Refresh archive completion status after reading a previous mission

--- koreader/rupertdash.koplugin/rupertdashboard.lua
+++ koreader/rupertdash.koplugin/rupertdashboard.lua
@@ -219,7 +219,7 @@ function Dashboard:buildStatusBar(width)
         align = "center",
         text(wifiIcon(), symbols(22)),
         HorizontalSpan:new{ width = 14 },
-                text("RUPERT  v28", bold(17)),
+        text("RUPERT  v29", bold(17)),
     }
     local right = HorizontalGroup:new{
         align = "center",
